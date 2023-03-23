469080bf48fd803fc2bc98bf784fcd30af266d6b
Kita Igniter <smtecsystem.469@gmail.com>
2023-03-23 20:25:19 -0300

    api!: saving users in memory

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuários");
+    return res.setHeader("Content-type", "application/json").end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "john@example"
+    });
+
     return res.end("Criação de usuários");
   }
-  
+
   return res.end("Hello World!");
 });
 

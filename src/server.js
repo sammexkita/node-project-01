3a93e526b3072e6a9afa5462ac5a4ffc7fc208d5
Kita Igniter <smtecsystem.469@gmail.com>
2023-03-23 19:29:04 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  return res.end("Hello World!");
+});
+
+server.listen(3333, () => {
+  console.log("Server running on port 3333 🚀");
+});
